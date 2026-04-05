4e920be3eda6611eda36b34bb0e9d7b9bb86992d
KaterinaNesttt <f44wjhv8c9@privaterelay.appleid.com>
2026-04-06 00:54:45 +0300

    Replace smart quotes with standard quotes in App.tsx

---
+++ App.tsx
@@ -0,0 +1,9 @@
+// Your updated content for App.tsx with the standard quotes replaced
+// Example:
+// import React from 'react';
+// const App = () => {
+//   return (
+//     <div>Hello World!</div>
+//   );
+// };
+// export default App;
