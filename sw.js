b63953ebe00b368aed6bc279bf8ff72c49320426
KaterinaNesttt <f44wjhv8c9@privaterelay.appleid.com>
2026-04-06 10:12:59 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// service worker file content with standard quotes.
-// Example:
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
-});
